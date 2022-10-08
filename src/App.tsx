0467453b9fdae2a047942fd59be404d19f8306ce
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-10-08 17:16:13 +0200

    page 'EmptyCart' has been added

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import { Routes, Route } from "react-router-dom";
 
 import "./scss/app.scss";
 import { Cart } from "./pages/Cart/Cart";
+import { useAppSelector } from "./hooks/useAppSelector";
+import { EmptyCart } from "./pages/EmptyCart/EmptyCart";
 
 function App() {
+  const { products } = useAppSelector((state) => state.cartSlice);
+
   return (
     <div className="wrapper">
       <Header />
@@ -14,7 +18,10 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/react-pizza-v2" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/cart"
+              element={products.length ? <Cart /> : <EmptyCart />}
+            />
           </Routes>
         </div>
       </div>
